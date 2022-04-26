2bf7d75927f8004022ea45ae4e8b5d1415bfb65c
JubalThang <jubalthang@gmail.com>
2022-04-26 10:49:26 -0500

    Done with models hopefully

--- app/models/church.rb
+++ app/models/church.rb
@@ -1,6 +1,7 @@
 class Church < ApplicationRecord
-  
+  validates :name, presence: true
   
   belongs_to :user
-  has_many :departments
+  has_many :departments, dependent: :destroy
+  has_many :items, through: :departments , dependent: :destroy
 end
